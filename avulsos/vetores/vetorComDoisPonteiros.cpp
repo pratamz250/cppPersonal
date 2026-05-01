e7b44bd14ffdee4bef4968b8d5b67d0baf489209
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-05-01 13:31:20 -0300

    Vetor com dois ponteiros

---
+++ avulsos/vetores/vetorComDoisPonteiros.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define REP(i, a, b) for(int i=a; i<b; i++)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vf = vector<float>;
+using vd = vector<double>;
+constexpr ll oo { 1LL << 62 };
+constexpr ll PRIME { 1'000'000'007 };
+constexpr double PI { acos(-1.0) };
+
+int main(){
+	//fastIO;
+		
+	vi as(6);
+
+	iota(as.begin(), as.end(), 0);
+
+	for(int i=0, j=5; i<j; i++, j--)
+		cout << as[i] << " " << as[j] << endl;
+
+	cout << endl;
+	return 0;
+}
