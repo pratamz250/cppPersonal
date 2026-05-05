9725ada5158e2a1287e5f3a3f4ff313f0723af2c
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-05-04 21:07:57 -0300

    Terminar busca binária depois

---
+++ avulsos/vetores/buscaBinaria.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	int n = 100, target = 20;
+
+	vector<int>v(n);
+
+	iota(v.begin(), v.end(), 0);
+
+	for(int i=0; i<100; i++){
+		
+	}
+
+	cout << endl;
+	return 0;
+}
