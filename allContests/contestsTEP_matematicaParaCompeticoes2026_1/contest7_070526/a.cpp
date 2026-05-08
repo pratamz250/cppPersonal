ca3ece8713221b3b80ebc5a94331611d6c55ec60
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-05-07 22:28:45 -0300

    Contest de hoje

---
+++ allContests/contestsTEP_matematicaParaCompeticoes2026_1/contest7_070526/a.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define REP(i, a, b) for(int i=a; i<b; i++)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vf = vector<float>;
+using vd = vector<double>;
+using vc = vector<char>;
+constexpr ll oo { 1LL << 62 };
+constexpr ll PRIME { 1'000'000'007 };
+constexpr double PI { acos(-1.0) };
+
+int main(){
+	fastIO;
+		
+	int t;
+	cin >> t;
+
+	while(t--){
+		int n, k, ans=0;
+
+		cin >> n >> k;
+
+		vi as(n);
+
+		REP(i, 0, n) cin >> as[i];
+
+		sort(as.begin(), as.end(), less<int>());
+
+		for(int i=as.size()-1; i>=1; i--){
+			//ans += abs(as[i] - k);
+			ans += abs((as[i] - k)/as[0]);
+		}
+		cout << ans << endl;
+	}
+
+	//cout << endl;
+	return 0;
+}
