61d471974034d0709786cc849284e74320a1600b
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-06-28 17:19:06 -0300

    Uso

--- usefullScripts/deleteAllEmptyLines.cpp
+++ usefullScripts/deleteAllEmptyLines.cpp
@@ -2,33 +2,39 @@
 #include <fstream>
 #include <string>
 
-int main() {
-	std::string entrada = "o3.txt";
-	std::string saida = "o5.txt";
+using namespace std;
 
-	std::ifstream in(entrada);
-	std::ofstream out(saida);
+int main(int argc, char *argv[]) {
+	if(argc != 3){
+		cout << "Error. Run: $ deleteAllEmptyLines.x <entrada> <saida>" << endl;
+		exit(1);
+	}
+
+	string entrada = argv[1];
+	string saida = argv[2];
+
+	ifstream in(entrada);
+	ofstream out(saida);
 
 	if (!in.is_open()) {
-		std::cerr << "Erro ao abrir o arquivo de entrada.\n";
+		cerr << "Erro ao abrir o arquivo de entrada.\n";
 		return 1;
 	}
 
 	if (!out.is_open()) {
-		std::cerr << "Erro ao criar o arquivo de saída.\n";
+		cerr << "Erro ao criar o arquivo de saída.\n";
 		return 1;
 	}
 
-	std::string linha;
-	while (std::getline(in, linha)) {
-		//Remove espaços em branco no início e no fim
+	string linha;
+	while (getline(in, linha)) {
 		size_t inicio = linha.find_first_not_of(" \t\r");
 		size_t fim = linha.find_last_not_of(" \t\r");
 
-		if (inicio != std::string::npos && fim != std::string::npos) {
+		if (inicio != string::npos && fim != string::npos) {
 			linha = linha.substr(inicio, fim - inicio + 1);
 		} else {
-			linha.clear();   //Linha contém apenas espaços
+			linha.clear();
 		}
 
 		if (!linha.empty()) {
@@ -36,7 +42,7 @@ int main() {
 		}
 	}
 
-	std::cout << "Arquivo '" << saida << "' criado sem as linhas em branco.\n";
+	cout << "Arquivo '" << saida << "' criado sem as linhas em branco.\n";
 
 	return 0;
 }
