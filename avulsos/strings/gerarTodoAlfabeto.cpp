82194df63bd4afe12476d0c324b7bfa75d030f56
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-07-14 21:59:36 -0300

    Uso

--- avulsos/strings/gerarTodoAlfabeto.cpp
+++ avulsos/strings/gerarTodoAlfabeto.cpp
@@ -5,11 +5,17 @@ using namespace std;
 int main(){
 	char start = 'a';
 	string alfabeto;
+	int count = 1;
 
 	for(int i=start; i<='z'; i++){
 		alfabeto.push_back(i);
 	}
-	cout << alfabeto << endl;
+
+	for(auto e : alfabeto){
+		cout << e << "|" << count << " ";
+		count++;
+	}
 	
+	cout << endl;
 	return 0;
 }
