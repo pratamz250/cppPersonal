af1f2cfe44441e9977744bd555fa77935d2ac624
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-08-18 16:08:25 -0300

    Uso

--- treinos/codeForces/bfs/twoButtons.cpp
+++ treinos/codeForces/bfs/twoButtons.cpp
@@ -11,12 +11,13 @@ using vll = vector<ll>;
 using vf = vector<float>;
 using vd = vector<double>;
 using vc = vector<char>;
+using qi = queue<int>;
 constexpr ll oo { 1LL << 62 };
 constexpr ll PRIME { 1'000'000'007 };
 constexpr double PI { acos(-1.0) };
 
 int solve();
-int bfs(int s, int m);
+int bfs(int n, int m);
 
 int main(){
 	fastIO;
@@ -30,41 +31,39 @@ int main(){
 int solve(){
 	int n, m;
 
-	cin >> n >> m; //bfs comeca em n e termina em m
+	cin >> n >> m;
 
 	cout << bfs(n, m);
 
 	return 0;
 }
 
-int bfs(int s, int m){
+int bfs(int n, int m){
 	const int MAX = 20005;
-	vector<int> dist(MAX, -1);
+	vi dist(MAX, -1);
 
-	queue<int> q;
-	q.push(s);
-	dist[s] = 0;
+	qi q;
+	q.push(n);
+	dist[n] = 0;
 
 	while(!q.empty()){
 		int v = q.front();
 		q.pop();
 
-		if(v == m) 
+		if(v == m)
 			return dist[v];
 
-		int u = v - 1;
-		if(u >= 0 and dist[u] == -1){
-			dist[u] = dist[v] + 1;
-			q.push(u);
+		int v1 = v * 2;
+		if(v1 < MAX and dist[v1] == -1){
+			dist[v1] = dist[v] + 1;
+			q.push(v1);
 		}
 
-		u = 2 * v;
-		if(u < MAX and dist[u] == -1){
-			dist[u] = dist[v] + 1;
-			q.push(u);
+		int v2 = v - 1;
+		if(v2 >= 0 and dist[v2] == -1){
+			dist[v2] = dist[v] + 1;
+			q.push(v2);
 		}
-
 	}
-
 	return -1;
 }
